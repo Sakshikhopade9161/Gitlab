92029c649329c5f6eef77a7bdb1d9444c9a5efff
Sakshikhopade9161 <Sakshikhopade9161@gmail.com>
2026-08-10 10:06:36 +0530

    Inital Project Structure

---
+++ Marks.py
@@ -0,0 +1,7 @@
+Java=int(input("Enter Marks of Java:"))
+FDS=int(input("Enter Marks of FDS:"))
+Python=int(input("Enter Marks of Python:"))
+OS=int(input("Enter Marks of OS:"))
+print(f"Marks of Java:{Java},Marks of FDS:{FDS},Marks of Python:{Python},Marks of OS:{OS}")
+
+
